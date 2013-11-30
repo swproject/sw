f1c8b7007c421a48a27531e7439c2e831ba007e3
unknown <vincent.marsy@cse.tamu.edu>
2013-11-29 23:40:37 -0600

    modifs

--- projet_softEng/src/projet_softEng/Test1.java
+++ projet_softEng/src/projet_softEng/Test1.java
@@ -8,18 +8,23 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.Theories;
 
 /**
  * @author Vincent
  *
  */
 public class Test1 {
+	
+	
+	private TheClassToTest m_object;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		m_object = new TheClassToTest();
 	}
 
 	/**
@@ -27,6 +32,7 @@ public class Test1 {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	/**
@@ -36,5 +42,14 @@ public class Test1 {
 	public void testMultiplyBy2() {
 		fail("Not yet implemented");
 	}
+	
+	/**
+	 * Test method for {@link projet_softEng.TheClassToTest#DivideByNumberFromInternet(int)}.
+	 */
+	@Test
+	public void testDivideByNumberFromInternet() {
+		m_object.DivideByNumberFromInternet(28);
+	}
+	
 
 }
--- projet_softEng/src/projet_softEng/TheClassToTest.java
+++ projet_softEng/src/projet_softEng/TheClassToTest.java
@@ -1,10 +1,19 @@
 package projet_softEng;
+import java.util.Random;
 
 public class TheClassToTest {
 	
-	int multiplyBy2(int x)
+	int MultiplyBy2(int x)
 	{		
 		return x*2;
+	}
+	
+	int DivideByNumberFromInternet(int x)
+	{
+		Random random = new Random();
+		int divisor = (random.nextInt(4)-3);
+		System.out.println("divisor: "+ divisor);
+		return x/(divisor);
 	}
 	
 
